27e4a02b8da796cfeafc6599fe503f5dbee8ca97
Lucas <lgs.08lucas@gmail.com>
2025-01-21 20:59:04 -0300

    Criado Funcionalidade de Sortear o amigo(a)

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 let names = [];
 const nameEl = document.querySelector("#amigo");
 const listEl = document.querySelector("#listaAmigos");
+const resultEl = document.querySelector("#resultado");
 
 function insertName() {
 	let name = nameEl.value;
 	if (nameIsValid(name)) {
 		names.push(name);
 		nameEl.value = "";
-	} else window.alert("Por favor, insira um nome válido!");
+	} else window.alert("Por favor, insira um nome.");
 	printName(name);
 }
 
@@ -21,4 +22,11 @@ function printName(name) {
 	listEl.appendChild(li);
 }
 
-function sortName() {}
+function sortName() {
+	if (names.length == 0) {
+		window.alert("Não há nenhum nome na lista!");
+		return;
+	}
+	const sortedIndex = parseInt(Math.random() * names.length);
+	resultEl.innerHTML =`O(a) escolhido(a) é ${names[sortedIndex]}`;
+}
